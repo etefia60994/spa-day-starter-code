8dfa3b683bcaec293afef90169dc10f5379a4f0d
etefia60994 <etefia60994@gmail.com>
2019-12-19 18:55:05 -0600

    Files Made

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,10 @@
+package org.launchcode.spaday.controllers;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+}
